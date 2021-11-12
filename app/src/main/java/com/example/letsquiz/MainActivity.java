4de8561b550ad18897c205c35b9bedad13dbb907
mayank <chaudharysinghmayank00gmail.com>
2021-11-12 13:10:38 +0530

    new changes regarding authentication have been made

--- app/src/main/java/com/example/letsquiz/MainActivity.java
+++ app/src/main/java/com/example/letsquiz/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.letsquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,30 +9,55 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    private TextInputLayout username, password;
+    private TextInputEditText username, password;
     private ProgressBar bar;
     private Object TextInputLayout;
     private FirebaseAuth mAuth;
     private TextView register, register1;
+    private AppCompatButton signInButton;
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser!=null){
+            reload();
+
+        }
+    }
+
+    private void reload() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+
         //Get reference of all the required views
-        username = (TextInputLayout) findViewById(R.id.Email);
-        password = (TextInputLayout) findViewById(R.id.pwd);
-        bar = (ProgressBar) findViewById(R.id.progressBar);
-        register = findViewById(R.id.registerUser);
+        username =  findViewById(R.id.email);
+        password =  findViewById(R.id.pwd);
+        bar =  findViewById(R.id.progress_bar);
+        signInButton= findViewById(R.id.sign_in_button);
+
+
+
 
         /*setting intent on register textview 1 to go to
           register activity */
+        register = findViewById(R.id.register_user);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
          /*setting intent on register textview 2 to go to
           register activity */
-        register1 = findViewById(R.id.registerUser2);
+        register1 = findViewById(R.id.register_user2);
         register1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/letsquiz/RegisterActivity.java
+++ app/src/main/java/com/example/letsquiz/RegisterActivity.java
@@ -1,60 +1,99 @@
 package com.example.letsquiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private String[] user= {"Teacher","Student"};
+    // array that can user type
+    private String[] user = {"Teacher", "Student"};
+
+    // Fields initialisation
     private TextView loginUser, loginUser2;
+    private TextInputEditText username, email, password, confirmPassword;
+    private ProgressBar bar;
+    private AppCompatButton signUpButton;
+    private Spinner userSelection;
+
+    //firebase auth initialisation
     private FirebaseAuth mAuth;
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void reload() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.pwd);
+        confirmPassword = findViewById(R.id.confirm_pwd);
+
+
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
 
-        loginUser= findViewById(R.id.loginUser);
-        Intent goToLogin =new Intent(RegisterActivity.this,MainActivity.class);
-        loginUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(goToLogin);
-            }
+        loginUser = findViewById(R.id.loginUser);
+        Intent goToLogin = new Intent(RegisterActivity.this, MainActivity.class);
+        loginUser.setOnClickListener(v -> {
+            startActivity(goToLogin);
+
         });
-        loginUser2= findViewById(R.id.loginUser2);
-        loginUser2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(goToLogin);
-            }
-        });
+        loginUser2 = findViewById(R.id.loginUser2);
+        loginUser2.setOnClickListener(v -> {
+                    startActivity(goToLogin);
+                }
+        );
 
 
         // Take the instance of Spinner and
         // apply OnItemSelectedListener on it which
         // tells which item of spinner is clicked
-        Spinner userSelection= findViewById(R.id.spinner);
+
+        userSelection = findViewById(R.id.spinner);
         userSelection.setOnItemSelectedListener(this);
 
         // Create the instance of ArrayAdapter
         // having the list of courses
-        ArrayAdapter userArrayAdapter= new ArrayAdapter(this, android.R.layout.simple_spinner_item,user);
+        ArrayAdapter userArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, user);
 
 
         // set simple layout resource file
@@ -67,12 +106,52 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
         userSelection.setAdapter(userArrayAdapter);
     }
 
+    private void createUser() {
+        String userName = username.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPwd = password.getText().toString();
+        String userConfirmPwd = confirmPassword.getText().toString();
+        // String userType = userSelection.getTransitionName().toString();
+        if (TextUtils.isEmpty(userName)) {
+            username.setError("Username cannot be empty");
+            username.requestFocus();
+        } else if (TextUtils.isEmpty(userEmail)) {
+
+            email.setError("Email cannot be empty");
+        } else if (TextUtils.isEmpty(userPwd)) {
+
+            password.setError("Password cannot be empty");
+        } else if (TextUtils.isEmpty(userConfirmPwd)) {
+
+            confirmPassword.setError("Confirm password cannot be empty");
+        } else {
+            mAuth.createUserWithEmailAndPassword(userEmail, userPwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (task.isSuccessful()) {
+                        Toast.makeText(RegisterActivity.this, "User Register Successfully", Toast.LENGTH_SHORT).show();
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Registration ERROR!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void updateUI(FirebaseUser user) {
+
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
         // make toast of name of course
         // which is selected in spinner
-        Toast.makeText(getApplicationContext(),user[position],Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), user[position], Toast.LENGTH_LONG).show();
     }
 
     @Override
